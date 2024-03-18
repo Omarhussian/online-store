ecaaef89542ddc610e60a3730aabf9c66d62a05a
Alhussain <omarh3795@gmail.com>
2024-03-18 22:38:20 +0200

    comments to explain my code

--- src/Lib/Redux/store.js
+++ src/Lib/Redux/store.js
@@ -4,18 +4,23 @@ import storage from 'redux-persist/lib/storage';
 import reducers from './Slices';
 
 const persistConfig = {
-  key: 'root',
-  storage,
+  key: 'root', // Key for the persisted state in storage
+  storage, // Storage engine to use (default is localStorage)
   whitelist: ['CartSlice' , 'InventorySlice'],
 };
 
+// Create a persisted reducer with redux-persist
+
 const persistedReducer = persistReducer(persistConfig, reducers);
 
+
+// Configure Redux store with persisted reducer and custom middleware
+
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: persistedReducer, // Use the persisted reducer
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: false, // Disable serializable check for redux-persist
     }),
 });
 
