b4a2fb843b5bf4f5143133766ae33ef800d26b37
Alhussain <omarh3795@gmail.com>
2024-03-18 19:57:02 +0200

    adding the two lines

--- src/Components/PageSpecific/Home/ProductsList.jsx
+++ src/Components/PageSpecific/Home/ProductsList.jsx
@@ -7,7 +7,13 @@ const ProductsList = () => {
   return (
     <div className='flex h-screen flex-col justify-start items-center p-2  border-4 border-[#000000] border-double overflow-y-auto '>
       <h1 className={styles.header}>Fish Store</h1>
-      <h3 className={styles.SubHeader}> Our Products List</h3>
+      <h3 className={styles.SubHeader}>
+        <span className='absolute top-4 -left-10 w-[36px] h-[1px] bg-black'></span>
+        <span className='absolute top-4 -left-10 w-[36px] h-[1px] bg-black mt-1'></span>
+        Our Products List
+        <span className='absolute top-4 -right-10 w-[36px] h-[1px] bg-black'></span>
+        <span className='absolute top-4 -right-10 w-[36px] h-[1px] bg-black mt-1'></span>
+      </h3>
       <div className='w-full'>
         <ListComponent />
       </div>
