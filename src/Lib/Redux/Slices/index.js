fd6747c5fd44830dadfc05fb889c3657f670bcad
Alhussain <omarh3795@gmail.com>
2024-03-18 22:39:13 +0200

    adding comments

--- src/Lib/Redux/Slices/index.js
+++ src/Lib/Redux/Slices/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "@reduxjs/toolkit";
 import CartSlice from "./CartSlice";
 import InventorySlice from "./InventorySlice";
 
+// Combine multiple reducers into a single root reducer
 
 export default combineReducers({
     CartSlice,
