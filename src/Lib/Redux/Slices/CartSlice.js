e4a8387e971eba3c6c618544b79ecc4c3e79c0d1
Alhussain <omarh3795@gmail.com>
2024-03-18 22:35:54 +0200

    adding comments

--- src/Lib/Redux/Slices/CartSlice.js
+++ src/Lib/Redux/Slices/CartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// here dfinig initial state for the cart slice
 const initialState = {
-  items: [], 
+  items: [], // array to store items in the cart = 
 };
 
 export const cartSlice = createSlice({
@@ -17,10 +18,13 @@ export const cartSlice = createSlice({
         state.items.push({ ...action.payload, quantity: 1 });
       }
     },
+     // here are reducer function to remove an item from the cart which was not provided in ux but ready to use 
     removeFromCart: (state, action) => {
       const { id } = action.payload;
       state.items = state.items.filter(item => item.id !== id);
     },
+     // here are reducer function to clear the whole cart which was not provided in ux but ready to use 
+
     clearCart: state => {
       state.items = [];
     },
