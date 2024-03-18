43724f8706e3d7f1e8b1a6fe45b04a90625d4f53
Alhussain <omarh3795@gmail.com>
2024-03-18 22:33:09 +0200

    adding comments

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,8 +3,10 @@ import ProductsList from '../../Components/PageSpecific/Home/ProductsList'
 import Cart from '../../Components/PageSpecific/Home/Cart'
 import Inventory from '../../Components/PageSpecific/Home/Inventory'
 
+// here is home component renders the main layout of the home page
 
 const Home = () => {
+    // here is main container div with grid layout
 
   return (
     <div className='h-screen grid lg:grid-cols-[repeat(3,minmax(300px,1fr))] md:grid-cols-[repeat(2,minmax(300px,1fr))] '>
